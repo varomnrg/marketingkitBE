b173cb220a441fa0ee5f6bea4e5f280f0c1a4ca4
Varo <varomnrg@gmail.com>
2022-11-11 18:04:29 +0700

    setting .env file

--- config/database/db.js
+++ config/database/db.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 var Sequelize = require('sequelize')
-var db = new Sequelize('marketingkit','root','',{
+var db = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASS,{
     dialect:'mysql',
-    host:'localhost'
+    host:process.env.DB_HOST
 })
 
 module.exports = db;
